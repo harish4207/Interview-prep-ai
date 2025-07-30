692ce2db019418929df28075861ee67158419f31
Harish <satagopamharish@gmail.com>
2025-07-30 23:37:54 +0530

    first commit

---
+++ backend/controllers/aiController.js
@@ -0,0 +1,112 @@
+
+const { conceptExplainPrompt, questionAnswerPrompt } = require("../utils/prompts");
+const { GoogleGenAI } = require("@google/genai");
+
+
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+
+// @desc    Generate interview questions and answers using Gemini
+// @route   POST /api/ai/generate-questions
+// @access  Private
+const generateInterviewQuestions = async (req, res) => {
+  try {
+    const { role, experience, topicsToFocus, numberOfQuestions } = req.body;
+
+    if (!role || !experience || !topicsToFocus || !numberOfQuestions) {
+      return res.status(400).json({ message: "Missing required fields" });
+    }
+
+    const prompt = questionAnswerPrompt(role, experience, topicsToFocus, numberOfQuestions);
+
+    const response = await ai.models.generateContent({
+      model: "gemini-2.0-flash-lite",
+      contents: prompt,
+    });
+
+
+    let rawText = response.text;
+    // Optional: Clean the output if wrapped in ```json
+    const cleanedText = rawText
+      .replace(/^```json\s*/, "")
+      .replace(/```$/, "")
+      .trim();
+     
+//       data.forEach((item) => {
+//   item.answer = item.answer
+//     .replace(/\n{2,}/g, '\n')         // Remove double newlines
+//     .replace(/\n/g, ' ')              // Replace all newlines with space
+//     .replace(/\*\*/g, '')             // Remove markdown bold
+//     .replace(/^\s*\*\s*/gm, '')       // Remove bullet point asterisks at the beginning of lines
+//     .replace(/\s+/g, ' ')             // Normalize multiple spaces
+//     .trim();
+// });
+//     Parse JSON safely
+    const data = JSON.parse(cleanedText);
+
+
+
+
+
+
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to generate questions",
+      error: error.message,
+    });
+  }
+};
+/**
+ * @desc    Generate explains a interview question
+ * @route   POST /api/ai/generate-explanation
+ * @access  Private
+ */
+const generateConceptExplanation = async (req, res) => {
+  try {
+    const { question } = req.body;
+
+    if (!question) {
+      return res.status(400).json({ message: "Missing required fields" });
+    }
+
+    const prompt = conceptExplainPrompt(question);
+
+    const response = await ai.models.generateContent({
+      model: "gemini-2.0-flash-lite",
+      contents: prompt,
+    });
+
+    let rawText = response.text;
+
+    
+    // Optional: Clean the output if wrapped in ```json
+    const cleanedText = rawText
+      .replace(/^```json\s*/, "")
+      .replace(/```$/, "")
+      .trim();
+
+    // Parse JSON safely
+    const data = JSON.parse(cleanedText);
+
+// data.forEach((item) => {
+//   item.answer = item.answer
+//     .replace(/\n{2,}/g, '\n')         // Remove double newlines
+//     .replace(/\n/g, ' ')              // Replace all newlines with space
+//     .replace(/\*\*/g, '')             // Remove markdown bold
+//     .replace(/^\s*\*\s*/gm, '')       // Remove bullet point asterisks at the beginning of lines
+//     .replace(/\s+/g, ' ')             // Normalize multiple spaces
+//     .trim();
+// });
+
+
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to generate questions",
+      error: error.message,
+    });
+  }
+};
+
+
+module.exports = { generateInterviewQuestions, generateConceptExplanation };
